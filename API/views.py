9758b45306529cd9454226faae789d7d23892faf
FuentesFelipe <f.fuentes.pro@gmail.com>
2019-07-13 23:24:23 -0400

    Modificación en views.py modificando la clase REST

--- API/views.py
+++ API/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from http.client import SimpleHTTPRequestHandler, BaseHTTPRequestHandler, HTTPServer
+
 # Create your views here.
 
-
-class REST(APIView):
+class REST(APIView, BaseHTTPRequestHandler):
     authentication_classes = []
     permission_classes = []
+
+    def _send_cors_headers(self):
+      """ Sets headers required for CORS """
+      self.send_header("Access-Control-Allow-Origin", "*")
+
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+    
     def get(self, request, format=None):
         def str_join(*args):
             return ''.join(map(str, args))
