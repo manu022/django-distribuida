82ddbaee9baf0acc74930a0bdb3a6aac9e139c1b
Manuel <manuel.grandond@utem.cl>
2019-07-16 01:42:32 -0400

    algoritmo género

--- API/views.py
+++ API/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from Code import GenderIdentifier
 # Create your views here.
 
 
@@ -11,9 +12,14 @@ class REST(APIView):
     def get(self, request, format=None):
         def str_join(*args):
             return ''.join(map(str, args))
-        
+        gender_identifier = GenderIdentifier.GenderIdentifier("audio", "females.gmm", "males.gmm")
+        winner=gender_identifier.process()
+        if winner=='male':
+            winner='Hombre'
+        if winner=='female':
+            winner='Mujer'
         data = {
-            "edad":'Hola',
-            "genero":'Chao'
+            "edad":'Edad',
+            "genero":winner
         }
         return Response(data)
---
+++ Code/GenderIdentifier.py
@@ -0,0 +1,58 @@
+import os
+import pickle
+import warnings
+import numpy as np
+from Code import FeaturesExtractor
+
+warnings.filterwarnings("ignore")
+
+
+class GenderIdentifier:
+
+    def __init__(self, file_path, females_model_path, males_model_path):
+        self.path   = file_path
+        self.error                 = 0
+        self.total_sample          = 0
+        self.features_extractor    = FeaturesExtractor.FeaturesExtractor()
+        # load models
+        self.females_gmm = pickle.load(open(females_model_path, 'rb'))
+        self.males_gmm   = pickle.load(open(males_model_path, 'rb'))
+
+    def process(self):
+        files = self.get_file_paths(self.path)
+        # read the test directory and get the list of test audio files
+        for file in files:
+            self.total_sample += 1
+            print("%10s %8s %1s" % ("--> TESTING", ":", os.path.basename(file)))
+
+            vector = self.features_extractor.extract_features(file)
+            winner = self.identify_gender(vector)
+            print("%10s %3s %1s" %  ("+ IDENTIFICATION", ":", winner))
+
+        
+            print("----------------------------------------------------")
+        return winner
+
+        
+
+    def get_file_paths(self, path):
+        # get file paths
+        files   = [ os.path.join(path, f) for f in os.listdir(path) ]
+        return files
+
+    def identify_gender(self, vector):
+        # female hypothesis scoring
+        is_female_scores         = np.array(self.females_gmm.score(vector))
+        is_female_log_likelihood = is_female_scores.sum()
+        # male hypothesis scoring
+        is_male_scores         = np.array(self.males_gmm.score(vector))
+        is_male_log_likelihood = is_male_scores.sum()
+
+        print("%10s %5s %1s" % ("+ FEMALE SCORE",":", str(round(is_female_log_likelihood, 3))))
+        print("%10s %7s %1s" % ("+ MALE SCORE", ":", str(round(is_male_log_likelihood,3))))
+
+        if is_male_log_likelihood > is_female_log_likelihood: winner = "male"
+        else                                                : winner = "female"
+        return winner
+
+
