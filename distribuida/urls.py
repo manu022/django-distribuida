bae64ad0fef00537a98d899092f05bd89edfbcd4
Manuel <manuel.grandond@utem.cl>
2019-07-20 04:55:08 -0400

    correccion swagger

--- distribuida/urls.py
+++ distribuida/urls.py
@@ -24,7 +24,7 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Distribuida API')
 
 urlpatterns = [
-    url('', schema_view),
+    url('swagger/', schema_view),
     url('admin/', admin.site.urls),
     url('rest/', views.REST.as_view()),
     url('upload/', views.FileUploadView.as_view())
